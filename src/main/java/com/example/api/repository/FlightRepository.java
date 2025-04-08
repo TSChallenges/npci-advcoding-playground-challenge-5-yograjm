da33f1ffde4dd4a14351b027fc48a479b42ccb9c
yograjm <yograj.m@talentsprint.com>
2025-04-08 15:01:28 +0530

    Update FlightRepository.java

--- src/main/java/com/example/api/repository/FlightRepository.java
+++ src/main/java/com/example/api/repository/FlightRepository.java
@@ -1,10 +1,10 @@
-package com.example.api.repository;
+package com.hackerrank.api.repository;
 
-import com.example.api.model.Flight;
+import com.hackerrank.api.model.Flight;
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-
+  List<Flight> findByOrigin(String origin);
+  Optional<Flight> findById(Long id);
 }
-
-
