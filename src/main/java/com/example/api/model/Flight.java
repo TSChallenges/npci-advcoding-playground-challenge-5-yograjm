85ffc1b7d6a32e10ed25b690465c7626f58d1842
yograjm <yograj.m@talentsprint.com>
2025-04-08 15:00:57 +0530

    Update Flight.java

--- src/main/java/com/example/api/model/Flight.java
+++ src/main/java/com/example/api/model/Flight.java
@@ -1,12 +1,25 @@
-package com.example.api.model;
+package com.hackerrank.api.model;
 
+import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.util.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Data
+@Setter
 public class Flight {
     @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
+    private String flight;
+    private String origin;
+    private String destination;
+    private int[] speedSeries;
+   // private Date date;
 
 }
-
